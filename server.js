6e69c8c2c20eccd2df1fffadfa071ce76024d6bf
Dniel23 <121911325+Dniel23@users.noreply.github.com>
2025-06-25 19:51:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors =require('cors');
 const { MercadoPagoConfig, Payment } = require('mercadopago');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
